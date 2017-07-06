c0ba724ea1045c9a01da92f153d4f0d338a7602c
Max W <xypotion@gmail.com>
2017-07-05 19:48:36 -0700

    steps 3 and 4

--- Apptimize2/app/src/main/java/com/example/maxwunderlich/apptimize2/MainActivity.java
+++ Apptimize2/app/src/main/java/com/example/maxwunderlich/apptimize2/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.apptimize.Apptimize;
 import com.apptimize.ApptimizeTest;
 
@@ -17,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Apptimize.setup(this, "BySJdFrxNgVWdBG7M8TszBQgjfMBtSb");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action!!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
