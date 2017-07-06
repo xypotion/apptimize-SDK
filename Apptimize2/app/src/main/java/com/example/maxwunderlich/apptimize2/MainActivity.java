9702ecee80016bc647c45a888beb2186aadc5675
Max W <xypotion@gmail.com>
2017-07-05 19:37:30 -0700

    steps 1 and 2 of Apptimize SDK integration

--- Apptimize2/app/src/main/java/com/example/maxwunderlich/apptimize2/MainActivity.java
+++ Apptimize2/app/src/main/java/com/example/maxwunderlich/apptimize2/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.apptimize.Apptimize;
+import com.apptimize.ApptimizeTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
